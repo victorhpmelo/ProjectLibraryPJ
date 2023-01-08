ee8d5a9b269c9561b9be5e864a316cdf3c653b1b
Victor Hugo <102386614+victorhpmelo@users.noreply.github.com>
2023-01-08 11:53:39 -0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,8 @@
 import { Pool } from "pg";
 
 //conexao com o banco de dados usando uma "Pool"
-
-const connectionString = 'postgres://rpohvcib:WKApLH7Fs2aTI1awzBc0Utdnb3ZLjPLi@tuffi.db.elephantsql.com/rpohvcib'
+//inserir url do banco de dados
+const connectionString = ''
 
 
 const db = new Pool({connectionString})
